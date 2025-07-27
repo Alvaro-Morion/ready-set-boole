a024e7975c90628ea0242887be134d004cdd94fc
Alvaro Morion de eusebio <amorion-@c1r1s3.42madrid.com>
2025-07-27 19:31:05 +0200

    ignore

--- inc/rsb.h
+++ inc/rsb.h
@@ -7,5 +7,6 @@ unsigned int	adder(unsigned int a, unsigned int b);
 unsigned int    multiplier(unsigned int a, unsigned int b);
 unsigned int    gray_code(unsigned int a);
 bool            eval_formula(std::string proposition);
+void            print_truth_table(std::string formula);
 
 #endif
